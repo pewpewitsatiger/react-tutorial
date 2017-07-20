8bc287749943651ad193ef6904d5a29192533346
pewpewitsatiger <jaydon.montalvo@embelm21.com>
2017-07-20 18:40:12 -0400

    Created ColorBlock.js

---
+++ scripts/ColorBlock.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class ColorBlock extends Component {
+  render() {
+    return (
+      <div style="width: 200px; height 200px; background-color; {color}; text-align: center; display: table-cell; vertical-align: middle;"> </div>
+    );
+  }
+}
+
+ColorBlock.propTypes = {
+  children: React.PropTypes.node
+};
