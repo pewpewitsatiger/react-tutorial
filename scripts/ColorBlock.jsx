12ced0a188c3e2f5743a2f4d2ed4726ed61ca8e8
pewpewitsatiger <jaydon.montalvo@embelm21.com>
2017-07-28 00:05:12 -0400

    Trying out the click

--- scripts/ColorBlock.jsx
+++ scripts/ColorBlock.jsx
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
 
-export default class ColorBlock extends Component {
-  render() {
-    const color = this.props.color === 'random' ?
-      '#' + Math.floor(Math.random() * 16777215).toString(16) :
-      this.props.color;
+function getColor(value) {
+  if(value === 'random') {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  } else {
+    return value;
+  }
+}
 
+export default class ColorBlock extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: props.color
+    };
+  }
+
+  changeColor() {
+    this.setState({
+      color: getColor('random')
+    });
+  }
+
+  render() {
     const style = {
       width: '200px',
       height: '200px',
-      backgroundColor: color,
+      backgroundColor: this.state.color,
       textAlign: 'center',
       display: 'table-cell',
       verticalAlign: 'middle'
     };
 
     return (
-      <div style={style}>
+      <div style={style} onClick={this.changeColor}>
         { this.props.children }
       </div>
     );
